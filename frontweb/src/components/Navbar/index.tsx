266dc800844a75be7e4987ef393d820369c6dddd
eduardo.fgoncalves <efgcb@windowslive.com>
2022-03-20 00:17:29 -0300

    Login/Logut logic

--- frontweb/src/components/Navbar/index.tsx
+++ frontweb/src/components/Navbar/index.tsx
@@ -1,12 +1,55 @@
+import { useEffect, useState } from 'react';
+import history from 'util/history';
+import { getTokenData, isAuthenticated, removeAuthData, TokenData } from 'util/requests';
 import './styles.css';
 
+type AuthData = {
+  authenticated: boolean,
+  tokenData?: TokenData
+}
+
+
 const Navbar = () => {
+  const [authData, setAuthData] = useState<AuthData>({authenticated: false});
+
+    useEffect(() => {
+      if (isAuthenticated()) {
+      setAuthData({
+        authenticated: true,
+        tokenData: getTokenData()
+      });
+    }
+    else {
+      setAuthData({
+        authenticated: false
+      });
+    }
+    }, [])
+
+    const handleLogoutClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+        removeAuthData();
+        setAuthData({
+          authenticated: false
+        });
+        history.replace('/');
+    }
+
+
   return (
     <nav className="bg-primary main-nav">
       <a className="nav-logo-text" href="/movies">
         <h4>MovieFlix</h4>
       </a>
-      <button className="btn btn-primary btn-sair">SAIR</button>
+      {authData.authenticated? (
+        <button className="btn btn-primary btn-sair" onClick={handleLogoutClick}>SAIR</button>
+      ) : (
+        <button className="btn btn-primary btn-navbar-login">Login</button>
+      )
+    
+    }
+      
+      
     </nav>
   );
 };
--- frontweb/src/util/requests.ts
+++ frontweb/src/util/requests.ts
@@ -8,7 +8,7 @@ export const BASE_URL =  process.env.REACT_APP_BACKEND_URL ?? 'https://movieflix
 
 type Role = 'ROLE_OPERATOR' | 'ROLE_ADMIN';
 
-type TokenData = {
+export type TokenData = {
     exp: number;
     user_name: string;
     authorities: Role[];
@@ -66,6 +66,10 @@ export const saveAuthData = (obj : LoginResponse) => {
 export const getAuthData = () => {
     const str = localStorage.getItem(tokenKey) ?? "{}";
     return JSON.parse(str) as LoginResponse;   
+}
+
+export const removeAuthData = () => {
+    localStorage.removeItem(tokenKey);
 }
 
 
